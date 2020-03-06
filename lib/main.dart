67324d707cbe3017d9641e94f8702ab603f82ac8
Henrique Rosa <henrique.rosa@meta.com.br>
2020-03-06 14:43:20 -0300

    first commit com renderização da imagem em tela

---
+++ lib/main.dart
@@ -0,0 +1,157 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter OCR',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'OCR - Flutter'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File imageFile;
+
+  openGallery(BuildContext context) async {
+    var picture = await ImagePicker.pickImage(source: ImageSource.gallery);
+    this.setState(() {
+      imageFile = picture;
+    });
+    Navigator.of(context).pop();
+  }
+
+  openCamera(BuildContext context) async {
+    var picture = await ImagePicker.pickImage(source: ImageSource.camera);
+    this.setState(() {
+      imageFile = picture;
+    });
+    Navigator.of(context).pop();
+  }
+
+  void ShowActionSheet(BuildContext context) {
+    showCupertinoModalPopup(
+      context: context,
+      builder: (BuildContext context) => CupertinoActionSheet(
+          title: const Text('Escolha sua opção'),
+          actions: <Widget>[
+            CupertinoActionSheetAction(
+              child: const Text('Galeria'),
+              onPressed: () {
+                openGallery(context);
+              },
+            ),
+            CupertinoActionSheetAction(
+              child: const Text('Camera'),
+              onPressed: () {
+                openCamera(context);
+              },
+            )
+          ],
+          cancelButton: CupertinoActionSheetAction(
+            child: const Text('Cancel'),
+            isDefaultAction: true,
+            onPressed: () {
+              Navigator.pop(context, 'Cancelar');
+            },
+          )),
+    );
+  }
+
+  Widget ImageView() {
+    if (imageFile == null) {
+      return Text(
+        "Nenhuma Imagem Selecionada",
+        style: TextStyle(
+          color: Colors.black,
+          fontFamily: 'Raleway',
+          fontSize: 22.0,
+        ),
+      );
+    } else {
+      return Image.file(imageFile, height: 300, fit: BoxFit.fill);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                color: Colors.transparent,
+                width: MediaQuery.of(context).size.width,
+                height: 60,
+                child: FlatButton(
+                  shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(30.0),
+                  ),
+                  onPressed: () {
+                    ShowActionSheet(context);
+                  },
+                  color: Colors.blue,
+                  child: Text(
+                    "Selecionar Imagem",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Raleway',
+                      fontSize: 22.0,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5),
+                child: Container(
+                  color: Colors.transparent,
+                  width: MediaQuery.of(context).size.width,
+                  height: 60,
+                  child: FlatButton(
+                    shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(30.0),
+                    ),
+                    onPressed: () {},
+                    color: Colors.blue,
+                    child: Text(
+                      "Button",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Raleway',
+                        fontSize: 22.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: ImageView(),
+              ),
+            ],
+          ),
+        ));
+  }
+}
